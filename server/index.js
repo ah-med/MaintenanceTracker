14cdcdc3cdb063b1f78f6701229885fe59c4b27f
ah-med <30514950+ah-med@users.noreply.github.com>
2018-05-15 21:40:28 +0100

    Add create a request endpoint

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+
+import user from './routes/user';
+
+const upload = multer();
+const app = express();
+
+// parsing application/json
+app.use(bodyParser.json());
+
+// parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parsing multipart/form-data
+app.use(upload.array());
+
+app.use('/api/v1/users', user);
+
+// Setup a default route
+app.get('/api/v1/', (req, res) => res.status(200).send({
+  message: 'Welcome to Maintenance Tracker API. Listening on port 8000'
+}));
+
+export default app;
