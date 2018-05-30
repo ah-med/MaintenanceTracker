35623d2fb88eac3a5bb3656e64544889408ed868
ah-med <30514950+ah-med@users.noreply.github.com>
2018-05-30 12:39:39 +0100

    feat(admin): create admin approve a request endpoint - ensure endpoint test pass [Delivers #157874154]

--- server/routes/request.js
+++ server/routes/request.js
@@ -3,14 +3,18 @@ import adminController from '../controllers/admin';
 
 import verifyToken from '../middlewares/verifyToken';
 import verifyAdmin from '../middlewares/verifyAdmin';
+import verifyStatus from '../middlewares/verifyStatus';
 
 const { tokenVerification } = verifyToken;
 const { isAdmin } = verifyAdmin;
+const { isPending } = verifyStatus;
 
 const router = express.Router();
 
 // Get all requests in the application
 router.get('/', tokenVerification, isAdmin, adminController.getRequests);
 
+// Approve a request
+router.put('/:requestId/approve', tokenVerification, isAdmin, isPending, adminController.updateStatus);
 
 export default router;
