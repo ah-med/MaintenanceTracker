9889d0eba23aee4154de0fe1ad2b35bae24b52fc
ah-med <ahmedomosanya@gmail.com>
2018-08-04 09:31:53 +0100

    create alert boxes

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -27,3 +27,47 @@ function closeModal(id) {
     modalElement.className = modalElement.className.replace(" show", " hide")
 
 }
+
+function displayAlert(element, message) {
+	  // get the alert element
+      var alertElement =  document.getElementById(element);
+      
+      //insert message into alert element 
+      alertElement.innerHTML = '<span id="closealert" class="closealert">&times;</span>' + message;
+
+      // remove the class hidden
+      alertElement.classList.remove('hidden');
+      
+      // animate fade in
+      setTimeout(function() {
+      	alertElement.classList.remove('visuallyhidden');
+      }, 30); 
+      
+     // listens to the fade in transition, if it has happend 
+	 alertElement.addEventListener('transitionend', function() {
+     	// animate fade out
+     	alertElement.classList.add('visuallyhidden');
+        
+        // make the display: none after 5s of fadeout time
+        var t = setTimeout(function() {
+      		alertElement.classList.add('hidden');
+            console.log(alertElement.classList)
+      	}, 3000);
+       
+       // get the close alert
+       var closeAlert = document.getElementById('closealert');
+       
+       // clear timeout when button is click and replace class list
+       closeAlert.addEventListener('click', function() {
+       		clearTimeout(t);
+            alertElement.classList.add('hidden', 'visuallyhidden');
+       });
+       
+	 });
+}
+
+// var submitRequest = document.getElementById('createRequest');
+
+// submitRequest.addEventListener('click', displayAlert('alert-success', 'Request Created Successfully'));
+
+
