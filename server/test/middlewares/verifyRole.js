7870b8b8e4341099b27915553f89be90593412a0
ah-med <ahmedomosanya@gmail.com>
2018-08-22 19:26:30 +0100

    add test for verifyRole.user and verifyRole.admin

--- server/test/middlewares/verifyRole.js
+++ server/test/middlewares/verifyRole.js
@@ -64,3 +64,100 @@ describe('verifyRole.masterAdmin middleware', () => {
       });
   });
 });
+
+describe('verifyRole.admin', () => {
+  it('should return error for non admin token', (done) => {
+    // create an Master admin account
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'masterUser7', email: 'masterUser7@mail.com', password: '12345678', companyname: 'newCompany7'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        // login the Master admin account and get the token
+        chai.request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: 'masterUser7@mail.com', password: '12345678'
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            adminToken = bearer + res.body.data.token;
+            // use the token to create an account with 'USER' role
+            chai.request(app)
+              .post('/api/v1/users')
+              .set('Authorization', adminToken)
+              .send({
+                username: 'newUser7', email: 'newUser7@mail.com', password: '12345678'
+              })
+              .end((err, res) => {
+                expect(res.status).to.equal(201);
+                // login the new user
+                chai.request(app)
+                  .post('/api/v1/auth/login')
+                  .set('Authorization', adminToken)
+                  .send({
+                    email: 'newUser7@mail.com', password: '12345678'
+                  })
+                  .end((err, res) => {
+                    expect(res.status).to.equal(200);
+                    userToken = bearer + res.body.data.token;
+                    // use the token to get all the request in the application
+                    chai.request(app)
+                      .get('/api/v1/requests')
+                      .set('Authorization', userToken)
+                      .end((err, res) => {
+                        expect(res.body).to.have.property('error');
+                        expect(res.body.error).to.have.property('status');
+                        expect(res.body.error).to.have.property('title');
+                        expect(res.body.error).to.have.property('description');
+                        expect(res.status).to.equal(401);
+                        done();
+                      });
+                  });
+              });
+          });
+      });
+  });
+});
+
+describe('verifyRole.user', () => {
+  it('should return error for non user token', (done) => {
+    // create a Master admin account
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'masterUser8', email: 'masterUser8@mail.com', password: '12345678', companyname: 'newCompany8'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        // login the Master admin account and get the token
+        chai.request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: 'masterUser8@mail.com', password: '12345678'
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            adminToken = bearer + res.body.data.token;
+            // use the token to create a new request
+            chai.request(app)
+              .post('/api/v1/users/requests')
+              .set('Authorization', adminToken)
+              .send({
+                title: 'A new request',
+                details: 'Details of the new request'
+              })
+              .end((err, res) => {
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.have.property('status');
+                expect(res.body.error).to.have.property('title');
+                expect(res.body.error).to.have.property('description');
+                expect(res.status).to.equal(401);
+                done();
+              });
+          });
+      });
+  });
+});
