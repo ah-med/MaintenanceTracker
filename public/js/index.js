9d6e948fba8c7859813a53b8135925ed80cfa101
ah-med <ahmedomosanya@gmail.com>
2018-08-31 17:24:47 +0100

    add reDirectLogin function

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,21 @@ function toggleForms(id) {
         displayElement(id, 'block');
         displayElement('signup', 'none');
     }
-    if ( id === 'signup') {
+    if (id === 'signup') {
         displayElement(id, 'block');
         displayElement('login', 'none');
     }
 }
 
+function reDirectLogin(role) {
+    switch (role) {
+        case 'MASTER_ADMIN':
+            reDirect('./master_admin.html');
+            break;
+        case 'ADMIN':
+            reDirect('./admin.html');
+            break;
+        default:
+            reDirect('./user.html');
+    }
+}
